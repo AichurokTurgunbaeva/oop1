3f5368d8d93a0c8ca511c70e979659d16f83f68e
Notnik_KG <tasnim0800@gmail.com>
2022-02-01 12:17:25 +0600

    Initial commit

---
+++ src/com/company/Adam.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Adam {
+    String aty;
+    int jashy;
+    int boyu;
+    int salmagy;
+    String familiyasi;
+
+    void demAlat() {
+        System.out.println(aty + " dem alip jatat");
+    }
+    void jashkaChigat() {
+        System.out.println(aty+ " bul jyly "+ jashy+ " jashka chigat" );
+    }
+    void tamakJeyt() {
+        System.out.println(aty+ " tamak jep jatat");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Adam Adam = new Adam();
+        Adam.aty = "Aychurok";
+        Adam.boyu = 163;
+        Adam.familiyasi = "Turgunbaeva";
+        Adam.jashy = 27;
+        Adam.salmagy = 49;
+        Adam.demAlat();
+
+        Adam Adam2 = new Adam();
+        Adam2.aty = "Ayzirek";
+        Adam2.boyu = 165;
+        Adam2.familiyasi = "Turgunbaeva";
+        Adam2.jashy = 25;
+        Adam2.salmagy = 50;
+        Adam2.jashkaChigat();
+
+        Adam Adam3 = new Adam();
+        Adam3.aty = "Bekbolot";
+        Adam3.boyu = 175;
+        Adam3.familiyasi = "Turgunbaev";
+        Adam3.jashy = 22;
+        Adam3.salmagy = 60;
+        Adam3.tamakJeyt();
+    }
+}
